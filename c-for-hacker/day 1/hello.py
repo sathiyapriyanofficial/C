9df3da320baf317fd0abc43afc6950268a2e03f2
sathiyapriyanofficial <sathiyapriyan.official@gmail.com>
2026-08-01 17:00:09 +0000

    py docs

--- c-for-hacker/day 1/hello.py
+++ c-for-hacker/day 1/hello.py
@@ -2,12 +2,14 @@ import time
 
 last_time = []
 
+# RTOS not
+
 while True:
     time.sleep(1)
     last_time.append(time.time())
     if len(last_time) > 2:
         print("Time difference:", (last_time[-1] - last_time[-2])*100000)
-        
+
 
     
   
